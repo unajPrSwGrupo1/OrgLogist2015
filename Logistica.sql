2f66210b85572ab302dc70b1af8527fc6f685f20
IvanLuthje <luthjeivan@gmail.com>
2015-11-18 18:27:13 -0300

    Update Logistica.sql

--- Logistica.sql
+++ Logistica.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 -- Base de datos: `stocktransportelogistica`
 --
 CREATE DATABASE IF NOT EXISTS `StockTransporteLogistica` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `stocktransportelogistica`;
+USE `StockTransporteLogistica`;
 
 -- --------------------------------------------------------
 
