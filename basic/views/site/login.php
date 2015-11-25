1b02ad5d495c7843842388467a49737c06bb02b8
Jorge Gamez <jorgeoscargamez@gmail.com>
2015-11-24 22:13:35 -0300

    Login Suave

--- basic/views/site/login.php
+++ basic/views/site/login.php
@@ -2,120 +2,51 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 ?>
-
-<html lang="en-us">
-<meta charset="utf-8" />
+<html>
 <head>
-<title>Logeo</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-47210639-2', 'auto');
-  ga('send', 'pageview');
-</script>
-
-<style>
-@import url("http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css");
-@import url("http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700");
-*{margin:0; padding:0}
-.add1{margin:0 auto; width:720px}
-body{background:#294072; font-family: 'Source Sans Pro', sans-serif}
-.form{width:400px; margin:0 auto; background:#1C2B4A}
-.header{height:44px; background:#17233B}
-.header h2{height:44px; line-height:44px; color:#fff; text-align:center}
-.login{padding:0 20px}
-.login span.un{width:10%; text-align:center; color:#0C6; border-radius:3px 0 0 3px}
-.text{background:#12192C; width:90%; border-radius:0 3px 3px 0; border:none; outline:none; color:#999; font-family: 'Source Sans Pro', sans-serif} 
-.text,.login span.un{display:inline-block; vertical-align:top; height:40px; line-height:40px; background:#12192C;}
-
-.btn{height:40px; border:none; background:#0C6; width:100%; outline:none; font-family: 'Source Sans Pro', sans-serif; font-size:20px; font-weight:bold; color:#eee; border-bottom:solid 3px #093; border-radius:3px; cursor:pointer}
-ul li{height:40px; margin:15px 0; list-style:none}
-.span{display:table; width:100%; font-size:14px;}
-.ch{display:inline-block; width:50%; color:#CCC}
-.ch a{color:#CCC; text-decoration:none}
-.ch:nth-child(2){text-align:right}
-/*social*/
-.social{height:30px; line-height:30px; display:table; width:100%}
-.social div{display:inline-block; width:42%; color:#eee; font-size:12px; text-align:center; border-radius:3px}
-.social div i.fa{font-size:16px; line-height:30px}
-.fb{background:#3B5A9A; border-bottom:solid 3px #036} .tw{background:#2CA8D2; margin-left:16%; border-bottom:solid 3px #3399CC}
-/*bottom*/
-.sign{width:90%; padding:0 5%; height:50px; display:table; background:#17233B}
-.sign div{display:inline-block; width:50%; line-height:50px; color:#ccc; font-size:14px}
-.up{text-align:right}
-.up a{display:block; background:#096; text-align:center; height:35px; line-height:35px; width:50%; font-size:16px; text-decoration:none; color:#eee; border-bottom:solid 3px #006633; border-radius:3px; font-weight:bold; margin-left:50%}
-@media(max-width:480px){ .form{width:100%}}
-.links{width:400px; margin:0 auto; height:50px}
-.links a{display:block; float:left; height:40px; line-height:40px; padding:0 20px; background:#fff; text-decoration:none; margin-left:5px; font-size:16px; font-weight:bold; color:#09F}
-
-
-</style>
+<title>Logeo de Usuario</title>
 </head>
-<body>
-    
-    <div class="add1"><ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-9033581641953966" data-ad-slot="6617384532"></ins></div>   
-<div> 
-
-    <h1></h1>
-    
-</div>
-<script>
-
-</script>
-
-<!-- <div class="form"> -->
-<!-- <div class="header"><h2>Iniciar Sesi&oacuten</h2></div> -->
-<!-- <div class="login"> -->
-<!-- <form action=""> -->
-<!-- <ul> -->
-<!-- <li> -->
-<!-- <span class="un"><i class="fa fa-user"></i></span><input type="text" required class="text" placeholder="Nombre de Usuario"/></li> -->
-<!-- <li> -->
-<!-- <span class="un"><i class="fa fa-lock"></i></span><input type="password" required class="text" placeholder="Contrase&ntilde;a"/></li> -->
-<!-- <li> -->
-<!-- <p><a class="btn btn-lg btn-success" href="http://localhost/basic/web/index.php?r=site/menu">Iniciar</a></p> -->
-
-<!-- </li> -->
-<!-- <li><div class="span"><span class="ch"><input type="checkbox" id="r"> <label for="r">Recuerdame</label> </span> <span class="ch"><a href="#">Olvid&oacute Su Contrase&ntilde;a?</a></span></div></li> -->
-<!-- </ul> -->
-<!-- </form> -->
-<?php
+<body style="background-image: url(imagenes/FondoLogin.png)"  > 
+    <div>
+        <?php
 $form1 = ActiveForm::begin ( [ 
 		'method' => "post",
 		"id" => "login",
 		"enableClientValidation" => false,
 		"enableAjaxValidation" => true 
 ] );
-?>
-<div class="form">
-<div class="header"><h2>Iniciar Sesi&oacuten</h2></div>
-<div class="login">
-<form action="">
-<ul>
-<span class="un"><i class="fa fa-user"></i></span><?= $form1->field($model, "username")->input("text") ?>
-
-<span class="un"><i class="fa fa-lock"></i></span><?= $form1->field($model, "password")->input("password") ?> 
-
-<!-- <div class="form-group"><i class="fa fa-user"></i> -->
-  
-<!-- </div> -->
-<!-- <div class="form-group"><i class="fa fa-lock"></i> -->
- 
-<!-- </div> -->
-
-<?= Html::submitButton("Iniciar",  ["class" =>"btn btn-primary"])?>
-
-<div class="span"><span class="ch">
-<?= $form1 -> field($model,'rememberMe')->checkbox(['value'=>true])
-// ->label('Recuérdame')
-;?> 
-</span> <span class="ch"><a href="#">Olvid&oacute Su Contrase&ntilde;a?</a></span></div>
-<?php $form1->end() ?>
-
-</ul>
-</form>
+?>   
+    </div>
+    <div>
+   
+    </div>
+    <div class="container container-fluid">
+        <div class="row vertical-center-row">
+            <div class="text-center col-md-4 col-md-offset-4">      
+                <div class="form">
+                    <div class="header">
+                    <h2>Iniciar Sesi&oacuten</h2>
+                    </div>
+                    <div class="login">
+                         
+                            
+                    <?= $form1->field($model, "username")->input("text")->label('Usuario') ?>
+                    <?= $form1->field($model, "password")->input("password")->label('Contraseña') ?> 
+                    <?= Html::submitButton("Iniciar",  ["class" =>"btn btn-primary btn-block"])?>
+                        <div>
+        
+        
+                        </div>
+                    <?= $form1 -> field($model,'rememberMe')->checkbox(['value'=>true])//->label('Recuérdame');?> 
+                    <a href="#">Olvid&oacute Su Contrase&ntilde;a?</a> 
+                    <?php $form1->end() ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
+
+
